5504ecd8c5a92cf63f79afaa43f20f94729139d5
Claire <1119518763@qq.com>
2023-03-30 14:17:50 +0800

    init

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,15 @@
+import { Card } from "antd";
+import { Component } from "react";
+import './menu.less';
+
+class Menu extends Component {
+    render() {
+        return (
+            <Card bordered={false} className="menu">
+
+            </Card>
+        );
+    }
+}
+
+export default Menu;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import 'antd/dist/reset.css';
+import { Col, Row } from 'antd';
+import Menu from './components/menu/menu';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+  <Row>
+      <Col span={5} className="router"><Menu /></Col>
+      <Col span={19}>主题</Col>
+  </Row>
+);
 reportWebVitals();
